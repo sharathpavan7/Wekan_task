cd0100decacaa3a43cc36d9743590a9ef054cc86
Sharath7 <sharathpavan7@gmail.com>
2020-06-01 12:21:26 +0530

    commit - v0.0.1 api call related changes.

--- app/src/main/java/com/sharath/wekan_task/MainActivity.kt
+++ app/src/main/java/com/sharath/wekan_task/MainActivity.kt
@@ -2,11 +2,34 @@ package com.sharath.wekan_task
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sharath.wekan_task.api.ApiAdapter
+import com.sharath.wekan_task.api.ApiService
+import com.sharath.wekan_task.api.response.ApiResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getStationList()
+    }
+
+    fun getStationList() {
+        val request = ApiAdapter.retrofit().create(ApiService::class.java)
+        val call = request.getList()
+        call.enqueue(object : Callback<ApiResponse> {
+            override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
+                if (response.isSuccessful){
+
+                }
+            }
+            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
+
+            }
+        })
     }
 }
--- app/src/main/java/com/sharath/wekan_task/api/response/ApiResponse.kt
+++ app/src/main/java/com/sharath/wekan_task/api/response/ApiResponse.kt
@@ -3,5 +3,5 @@ package com.sharath.wekan_task.api.response
 import com.sharath.wekan_task.model.Station
 
 data class ApiResponse(
-    val result: List<Station>
+    val fuel_stations: List<Station>
 )
